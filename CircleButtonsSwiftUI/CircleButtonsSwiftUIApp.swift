24013a0204be5439de5d419e0987a9d79ae2ab78
Jim Hillhouse <portablefrontier@me.com>
2021-07-11 11:51:54 -0500

    Initial Commit

---
+++ CircleButtonsSwiftUI/CircleButtonsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleButtonsSwiftUIApp.swift
+//  CircleButtonsSwiftUI
+//
+//  Created by James Hillhouse IV on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CircleButtonsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircleButtonsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CircleButtonsSwiftUI
+//
+//  Created by James Hillhouse IV on 7/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
